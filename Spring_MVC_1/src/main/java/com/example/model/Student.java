7be21149e5d60e03af1872a32bc16e65dfadb840
Tokala-Rajesh <36854329+Tokala-Rajesh@users.noreply.github.com>
2020-04-20 16:50:30 +0530

    commit

---
+++ Spring_MVC_1/src/main/java/com/example/model/Student.java
@@ -0,0 +1,51 @@
+package com.example.model;
+
+public class Student
+{
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Student(String id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+
+}
---
+++ Spring_MVC_1/src/main/webapp/WEB-INF/view/student-form.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form:form action="processForm" method="post" modelAttribute="student">
+<label>First Name:</label><form:input path="firstName" /><br/>
+<label>Last Name:</label><form:input path="lastName"/><br/>
+<label>Email:</label><form:input path="email"/><br/>
+<input type="submit">
+</form:form>
+</body>
+</html>
